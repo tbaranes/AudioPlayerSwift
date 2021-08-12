5085089133828735e0744148a6544c9c4950cf83
Peter Easdown <info@pkclsoft.com>
2021-08-12 12:03:13 +1000

    Added detail to swift package spec.

--- Package.swift
+++ Package.swift
@@ -5,4 +5,22 @@ import PackageDescription
 
 let package = Package(
   name: "AudioPlayer"
+  
+  platforms: [
+    .iOS(.v12),
+    .tvOS(.v12),
+    .macOS(.v11)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(
+      name: "AudioPlayer",
+      targets: ["AudioPlayer"])
+    ],
+    targets: [
+      // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+      // Targets can depend on other targets in this package, and on products in packages this package depends on.
+      .target(
+        name: "AudioPlayer"
+    ]
 )
