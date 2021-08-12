6dd59e3aa8afe6b7b36eb344752e33cb5960caf0
Peter Easdown <info@pkclsoft.com>
2021-08-12 12:12:25 +1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
-  name: "AudioPlayer"
+  name: "AudioPlayer",
   
   platforms: [
     .iOS(.v12),
